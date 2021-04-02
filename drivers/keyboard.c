e0c7069be65589ed9ee86a9a7975c5ac2013c030
keith72632 <keithhorton30@outlook.com>
2021-04-02 11:23:00 -0500

    finished keyboard buffer and commands

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -7,6 +7,7 @@
 #define ENTER 0x1C
 #define TAB 0x0F
 #define BACKSPACE 0x0E
+#define SC_MAX 57
 
 u8int ctr = 0;
 
@@ -14,9 +15,9 @@ char tab[] = {
     ' ', ' ', ' ', ' '
 };
 
-char keyboard_buffer[256];
+static char key_buffer[256];
 
-u8int scancodes[] = {
+const char scancode_to_char[] = {
   '?', '?', '1', '2', '3', '4', '5',
   '6', '7', '8', '9', '0', '-', '=',
   '?', '?', 'Q', 'W', 'E', 'R', 'T',
@@ -28,21 +29,46 @@ u8int scancodes[] = {
   '?', ' '
 };
 
-
-void exectute_command(char *string)
+void print_nl() 
 {
-    if(strcmp(string, "EXIT") == 0)
-    {
-        printk("Exiting. Fuck off");
-        asm volatile("hlt");
-    } else {
-        printk(string);
-        printk("strcmp = ");
-        print_dec(strcmp(string, "EXIT"));
-        printk('\n');
+    int offset = move_offset_to_new_line(get_cursor());
+    if(offset >= MAX_ROWS * MAX_COLS * 2){
+        offset = scroll_ln(offset);
     }
+    set_cursor(offset);
 }
 
+void clear_screen() {
+    for(int i = 0; i < MAX_COLS * MAX_ROWS; i++){
+        print_char(' ', i * 2);
+    }
+    set_cursor(get_offset(0, 0));
+}
+
+int compare_string(char s1[], char s2[]) {
+    int i;
+    for (i = 0; s1[i] == s2[i]; i++) {
+        if (s1[i] == '\0') return 0;
+    }
+    return s1[i] - s2[i];
+}
+
+void execute_command(char *input)
+{
+    if(compare_string(input, "EXIT") == 0){
+        clear_screen();
+        printk("Stopping the CPU. Fuck Off\n");
+        asm volatile("hlt");
+    } else if(compare_string(input, "WHOAMI")==0){
+        printk("User: Keith\n");
+        printk(">");
+    }else{
+        printk("Unkown Command: ");
+        printk(input);
+        printk("\n>");
+    }
+
+}
 void append(char *string, char character, int *ptr)
 {
 
@@ -67,30 +93,29 @@ void print_backspace()
     }
 }
 
-void keyboard_callback()
-{
+static void keyboard_callback(registers_t *regs) {
     u8int scancode = port_byte_in(0x60);
-    int offset = get_cursor();
-    if(scancode > 100) return;
 
-    if(scancode == ENTER){
-        exectute_command(keyboard_buffer);
-        int offset = get_cursor();
-        int new_offset = move_offset_to_new_line(offset);
-        set_cursor(new_offset);
-        printk(">");
-    } else if(scancode == TAB) {
-        printk(tab);
+    if(scancode > SC_MAX) return;
 
-    } else if(scancode == BACKSPACE) {
-        print_backspace();
-        ctr-= 1;
-        keyboard_buffer[ctr] = '\0';        
-    } else {
-        append(keyboard_buffer, scancodes[(int)scancode], &ctr);
-        printk(keyboard_buffer);
+    if(scancode == BACKSPACE){
+        if(key_buffer[0] != '\0'){
+            print_backspace();
+            ctr -=1;
+            key_buffer[ctr] = '\0';
+        }
+    }else if(scancode == ENTER){
+        print_nl();
+        execute_command(key_buffer);
+        key_buffer[0] = '\0';
+        ctr = 0;
+    }else{
+        char letter = scancode_to_char[(int)scancode];
+        append(key_buffer, letter, &ctr);
+        key_buffer[ctr] = '\0';
+        char str[2] = {letter, '\0'};
+        printk(str);
     }
-    //print_dec(scancode);
 }
 
 void init_keyboard()
